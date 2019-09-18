f1cef1cf203b0ccab076719039b146f67651d677
niko-matses <nikolas.matses@gmail.com>
2019-09-18 16:38:28 -0400

    mount and unmount individual array items

--- src/toggle.js
+++ src/toggle.js
@@ -2,26 +2,36 @@ import React, { useState } from 'react';
 import { useTransition, animated } from 'react-spring';
 
 const Toggle = () => {
-  const [isToggled, setToggle] = useState(false);
-  const transition = useTransition(isToggled, null, {
-    from: { opacity: 0, position: 'absolute' },
+  const [items, setItems] = useState([{
+    letter: "N",
+    key: 1
+  }, {
+    letter: "i",
+    key: 2
+  }, {
+    letter: "k",
+    key: 3
+  }, {
+    letter: "o",
+    key: 4
+  }]);
+  const transition = useTransition(items, item => item.key, {
+    from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
   });
 
   return (
-    <div style={{
-      position: 'relative'
-    }}
-    >
-      {transition.map(({ item, key, props }) =>
-        item ? (
-          <animated.h1 style={props}>Hello</animated.h1>
-        ) : (
-            <animated.h1 style={props}>World</animated.h1>
-          )
-      )}
-      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+    <div>
+      {transition.map(({ item, key, props }) => (
+        <animated.h1 style={props} key={key}>
+          {item.letter}
+        </animated.h1>
+      ))}
+      <button onClick={() => setItems([{
+        letter: "N",
+        key: 1
+      }])}>Toggle</button>
     </div>
   );
 };
