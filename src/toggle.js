db2f2c45a8cb953ed6528c78eb229d621dde85e9
niko-matses <nikolas.matses@gmail.com>
2019-09-18 16:30:13 -0400

    transition between ttwo components, positioned absolute

--- src/toggle.js
+++ src/toggle.js
@@ -4,15 +4,23 @@ import { useTransition, animated } from 'react-spring';
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
   const transition = useTransition(isToggled, null, {
-    from: { opacity: 0 },
+    from: { opacity: 0, position: 'absolute' },
     enter: { opacity: 1 },
     leave: { opacity: 0 }
-  })
+  });
+
   return (
-    <div>
-      {transition.map(({ item, key, props }) => (
-        item && <animated.h1 key={key} style={props} >Hello</animated.h1>
-      ))}
+    <div style={{
+      position: 'relative'
+    }}
+    >
+      {transition.map(({ item, key, props }) =>
+        item ? (
+          <animated.h1 style={props}>Hello</animated.h1>
+        ) : (
+            <animated.h1 style={props}>World</animated.h1>
+          )
+      )}
       <button onClick={() => setToggle(!isToggled)}>Toggle</button>
     </div>
   );
