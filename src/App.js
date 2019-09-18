59b6ecda5d7048aca3d9d9ac24d954665dcdaf74
niko-matses <nikolas.matses@gmail.com>
2019-09-18 15:33:52 -0400

    fullscreen menu design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toggle from './toggle';
 import Nav from './Nav';
 import { useSpring, animated } from 'react-spring';
@@ -6,14 +6,18 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const navAnimation = useSpring({
+    transform: isNavOpen ? `translate3d(0, 0, 0) scale(1)` : `translate3d(100%, 0, 0) scale(0.6)`
+  });
   const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
   return (
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" />
-        {/* <button className="menu-button">Menu</button> */}
+        <button onClick={() => setNavOpen(!isNavOpen)} className="menu-button">Menu</button>
       </header>
-      <Nav />
+      <Nav style={navAnimation} />
       <main>
         {/* <Toggle /> */}
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { animated } from 'react-spring';
 
-const Nav = () => {
-  const [isToggled, setToggle] = useState(false);
-  const menuState = useSpring({
-    width: !isToggled ? '0%' : '100%'
-  });
-  const menuStyle = useSpring({
-    border: !isToggled ? '0px solid white' : '20px solid white',
-    padding: !isToggled ? '40px' : '20px'
-  })
+const Nav = ({ style }) => {
   return (
-    <nav>
-      <a class="menu-button" onClick={() => setToggle(!isToggled)}>Menu</a>
-      <animated.nav style={menuState}>
-        <animated.ul class="nav-items" style={menuStyle}>
-          <li><a href="#">home</a></li>
-          <li><a href="#">about</a></li>
-          <li><a href="#">work</a></li>
-          <li><a href="#">contact</a></li>
-        </animated.ul>
-      </animated.nav>
-    </nav>
+    <animated.div className="nav-wrapper" style={style}>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Store</a>
+      <a href="#">Tutorials</a>
+    </animated.div>
   );
 }
-export default Nav
+export default Nav;
