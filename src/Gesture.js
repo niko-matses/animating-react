fa1da77f67a55723dd757977f558093e8bfeb375
niko-matses <nikolas.matses@gmail.com>
2019-09-25 16:48:10 -0400

    set events using gestures

--- src/Gesture.js
+++ src/Gesture.js
@@ -4,14 +4,27 @@ import { animated, useSpring } from 'react-spring';
 
 
 const Gesture = () => {
-  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+  const [{ x }, set] = useSpring(() => ({ x: 0 }));
   const bind = useGesture(({ down, delta }) => {
-    set({ xy: down ? delta : [0, 0] });
-  })
+    if (down) {
+      set({ x: down ? delta[0] : 0 });
+    } else {
+      if (delta[0] > 400) {
+        set({ x: 500 });
+      } else if (delta[0] < -400) {
+        set({ x: 0 });
+      }
+    }
+  });
   return <animated.div
     style={{
-      transform: xy.interpolate((x, y) =>
-        `translate3d(${x}px, ${y}px, 0)`)
+      opacity: x.interpolate({
+        map: Math.abs,
+        range: [0, 400],
+        output: [1, 0]
+      }),
+      transform: x.interpolate((x) =>
+        `translate3d(${x}px, 0, 0)`)
     }}
     {...bind()}
     className="box"
